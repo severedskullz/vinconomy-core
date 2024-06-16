51decea617176a3c6867bf2e8cf863a4c566c544
unknown <severed_skullz@hotmail.com>
2024-06-16 15:38:05 -0500

    Adding I13N for RU

--- Viconomy/src/ItemTypes/ItemGachaBall.cs
+++ Viconomy/src/ItemTypes/ItemGachaBall.cs
@@ -41,30 +41,35 @@ namespace Viconomy.ItemTypes
         {
             ITreeAttribute treeAttr = slot.Itemstack.Attributes;
             ITreeAttribute contents = (TreeAttribute)treeAttr.GetTreeAttribute("Contents");
-            EntityAgent ent = player.Entity;
 
-            for (int i = 0; i < 6; i++)
+            if (contents != null)
             {
-                ItemStack blockStack = contents.GetItemstack($"Item{i}");
-                if (blockStack != null)
+                EntityAgent ent = player.Entity;
+
+                for (int i = 0; i < 6; i++)
                 {
-                    //Note: These console Writes were here because the spawning/giving of items was inconsistent. Sometimes it gave me nothing, some times it gave me the stack size AND spawned more.
-                    // If it ever happens again, this is where the problem was. Still no idea why/how. Hunch is the actual attribute stack size was getting modified by TryGiveItemstack before cloned it,
-                    // thus all subsequent calls gave me 0?
-                    //Console.WriteLine($"From Item: {blockStack.StackSize}");
-                    ItemStack newStack = blockStack.Clone();
-                    newStack.ResolveBlockOrItem(ent.World);
-                    //Console.WriteLine($"From New Item: {newStack.StackSize}");
-                    player.InventoryManager.TryGiveItemstack(newStack, true);
-                    //Console.WriteLine($"After Item: {blockStack.StackSize}");
-                    //Console.WriteLine($"After New Item: {newStack.StackSize}");
-                    if (newStack.StackSize > 0)
+                    ItemStack blockStack = contents.GetItemstack($"Item{i}");
+                    if (blockStack != null)
                     {
-                        ent.World.SpawnItemEntity(newStack, ent.SidedPos.XYZ.Add(0.0f, 0.5f, 0.0f), null);
+                        //Note: These console Writes were here because the spawning/giving of items was inconsistent. Sometimes it gave me nothing, some times it gave me the stack size AND spawned more.
+                        // If it ever happens again, this is where the problem was. Still no idea why/how. Hunch is the actual attribute stack size was getting modified by TryGiveItemstack before cloned it,
+                        // thus all subsequent calls gave me 0?
+                        //Console.WriteLine($"From Item: {blockStack.StackSize}");
+                        ItemStack newStack = blockStack.Clone();
+                        newStack.ResolveBlockOrItem(ent.World);
+                        //Console.WriteLine($"From New Item: {newStack.StackSize}");
+                        player.InventoryManager.TryGiveItemstack(newStack, true);
+                        //Console.WriteLine($"After Item: {blockStack.StackSize}");
+                        //Console.WriteLine($"After New Item: {newStack.StackSize}");
+                        if (newStack.StackSize > 0)
+                        {
+                            ent.World.SpawnItemEntity(newStack, ent.SidedPos.XYZ.Add(0.0f, 0.5f, 0.0f), null);
+                        }
+
                     }
-                        
                 }
             }
+
             slot.TakeOut(1);
             slot.MarkDirty();
         }
